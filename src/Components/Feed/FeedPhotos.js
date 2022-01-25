15a18670c1678b3ca66e9f8166387527cc3aef6c
Willerson <willerson.miranda@gmail.com>
2022-01-25 19:32:27 -0300

    scroll infinito

--- src/Components/Feed/FeedPhotos.js
+++ src/Components/Feed/FeedPhotos.js
@@ -6,17 +6,19 @@ import Loading from '../Helper/Loading';
 import styles from './FeedPhotos.module.css';
 import FeedPhotosItem from './FeedPhotosItem';
 
-const FeedPhotos = ({ setModalPhoto }) => {
+const FeedPhotos = ({ page, user, setModalPhoto, setInfinite }) => {
   const { data, loading, error, request } = useFetch();
 
   React.useEffect(() => {
     async function fetchPhotos() {
-      const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
-      const { json } = await request(url, options);
-      console.log(json);
+      const total = 3;
+      const { url, options } = PHOTOS_GET({ page, total, user });
+      const { response, json } = await request(url, options);
+      if (response && response.ok && json.length < total) setInfinite(false);
+      console.log('Request, ', json);
     }
     fetchPhotos();
-  }, [request]);
+  }, [request, user, page, setInfinite]);
   console.log(data);
 
   if (error) return <Error error={error} />;
