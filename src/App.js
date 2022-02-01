b63c4bcaf433c916e9fb28d1646a7cad7c41af57
Willerson Miranda <willerson.miranda@gmail.com>
2022-01-31 23:48:01 -0300

    Perfil usuario

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Footer from './Components/Footer';
-import Header from './Components/Header';
-import ProtectecRoute from './Components/Helper/ProtectecRoute';
-import Home from './Components/Home';
-import Login from './Components/Login/Login';
-import Photo from './Components/Photo/Photo';
-import User from './Components/User/User';
-import { UserStorage } from './UserContext';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import ProtectecRoute from "./Components/Helper/ProtectecRoute";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login";
+import Photo from "./Components/Photo/Photo";
+import User from "./Components/User/User";
+import UserProfile from "./Components/User/UserProfile";
+import { UserStorage } from "./UserContext";
 
 const App = () => {
   return (
@@ -28,6 +29,7 @@ const App = () => {
               }
             />
             <Route path="foto/:id" element={<Photo />} />
+            <Route path="perfil/:user" element={<UserProfile />} />
           </Routes>
           <Footer />
         </UserStorage>
