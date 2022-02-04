5bc119b4b9ab4747f1ba3e4b3fb76a96c84001b8
Will Miranda <willerson.miranda@gmail.com>
2022-02-03 23:10:36 -0300

    PropTypes

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import FeedModal from './FeedModal';
-import FeedPhotos from './FeedPhotos';
+import React from "react";
+import FeedModal from "./FeedModal";
+import FeedPhotos from "./FeedPhotos";
+import PropTypes from "prop-types";
 
 const Feed = ({ user }) => {
   const [modalPhoto, setModalPhoto] = React.useState(null);
@@ -22,11 +23,11 @@ const Feed = ({ user }) => {
         }
       }
     }
-    window.addEventListener('wheel', infiniteScroll);
-    window.addEventListener('scroll', infiniteScroll);
+    window.addEventListener("wheel", infiniteScroll);
+    window.addEventListener("scroll", infiniteScroll);
     return () => {
-      window.removeEventListener('wheel', infiniteScroll);
-      window.removeEventListener('scroll', infiniteScroll);
+      window.removeEventListener("wheel", infiniteScroll);
+      window.removeEventListener("scroll", infiniteScroll);
     };
   }, [infinite]);
 
@@ -46,6 +47,17 @@ const Feed = ({ user }) => {
       ))}
     </div>
   );
+};
+
+Feed.defaultProps = {
+  user: 0,
+};
+
+Feed.propTypes = {
+  user: PropTypes.oneOfType([
+    PropTypes.string.isRequired,
+    PropTypes.number.isRequired,
+  ]),
 };
 
 export default Feed;
